b2de28acf069b835b167d3563cbb11501da29940
Ross Wardrup <minorsecond@gmail.com>
2021-02-06 10:09:44 -0700

    Capture bpq call sign before requresting zip code

--- wx.py
+++ wx.py
@@ -1,12 +1,18 @@
+#!/bin/python
 # Get wx for zip code
 
 import requests
 import configparser
 
+import os
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+config_path = os.path.join(dir_path, "credentials.txt")
+
 api_key = "NULL"
 config = configparser.ConfigParser()
 try:
-    config.read("credentials.txt")
+    config.read(config_path)
     api_key = config['DEFAULT']['api_key']
 except KeyError:
     print("You must first create a credentials file named credentials.txt that adheres to the following format:\n")
@@ -15,11 +21,14 @@ except KeyError:
 # base_url variable to store url
 base_url = "http://api.openweathermap.org/data/2.5/weather?"
 
+call = input()
+
 if api_key != "NULL":
     zip_code = input("Enter zip code:")
     complete_url = base_url + "appid=" + api_key + "&q=" + zip_code
     response = requests.get(complete_url)
     x = response.json()
+    print(zip_code)
 
     if x["cod"] != "404":
         y = x["main"]
@@ -31,12 +40,12 @@ if api_key != "NULL":
         z = x["weather"]
         wx_desc = z[0]["description"]
 
-        print(f"The current wx conditions in {zip_code} are:")
-        print(f"Temperature:\t{current_temp}°F")
+        print(f"Welcome,{call}. The current wx conditions in {zip_code} are:")
+        print(f"Temperature:\t\t{current_temp}°F")
         print(f"Feels like:\t\t{feels_like}°F")
         print(f"Pressure:\t\t{current_pressure} inches of mercury")
         print(f"Humidity:\t\t{current_humidity}%")
-        print(f"Wx description: {wx_desc}")
+        print(f"Wx description:\t\t{wx_desc}")
 
     else:
-        print("Zip code not found.")
+        print(f"Zip code {zip_code} not found.")
