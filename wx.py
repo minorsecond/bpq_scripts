70cfefbb9a7b02e19ec58d1f5809eaf2b8f06612
Ross Wardrup <minorsecond@gmail.com>
2021-02-06 10:41:16 -0700

    Get city name correctly

--- wx.py
+++ wx.py
@@ -3,7 +3,6 @@
 
 import requests
 import configparser
-
 import os
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -22,24 +21,33 @@ except KeyError:
 base_url = "http://api.openweathermap.org/data/2.5/weather?"
 
 call = input()  # capture BPQ call sign that is sent
-
+zip_code = None
+tries = 0
 if api_key != "NULL":
-    zip_code = input("Enter zip code:").strip()
-    complete_url = base_url + "appid=" + api_key + "&q=" + zip_code
+    while zip_code is None or len(zip_code) != 5 and type(zip_code) != int:
+        zip_code = input("Enter zip code:").strip()
+        tries += 1
+        if tries == 3:
+            print("Too many invalid entries.\nExiting the program...")
+            exit()
+
+    complete_url = f"{base_url}zip={zip_code}&appid={api_key}"
     response = requests.get(complete_url)
     x = response.json()
+    city_name = x["name"]
+    lat = x['coord']['lat']
+    lon = x['coord']['lon']
 
     if x["cod"] != "404":
         y = x["main"]
-
-        current_temp = round(1.8 * (y["temp"]-273))
-        feels_like = round(1.8 * (y["feels_like"]-273))
+        current_temp = round(1.8 * (y["temp"]-273) + 32)
+        feels_like = round(1.8 * (y["feels_like"]-273) + 32)
         current_pressure = round(y["pressure"]*.02953, 2)
         current_humidity = y["humidity"]
         z = x["weather"]
         wx_desc = z[0]["description"]
 
-        print(f"Welcome,{call}\nThe current wx conditions in {zip_code} are:")
+        print(f"Welcome,{call}\nThe current wx conditions in {city_name} are:")
         print(f"Temperature:\t\t{current_temp} F")
         print(f"Feels like:\t\t{feels_like} F")
         print(f"Pressure:\t\t{current_pressure} inches of mercury")
